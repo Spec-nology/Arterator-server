8bee299d37fcaddbaacb5d2d9f855ab914d4bf0e
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-12-06 23:22:06 +0000

    add cors and json functionality

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const app = require('./index');
 const port = process.env.PORT || 3000;
-
 const helpers = require('./helpers');
 
-app.listen(port, () => {
-  console.log('hey');
-});
+const promptRoutes = require('./routes/prompts.js');
+app.use('/prompt', promptRoutes);
 
 app.get('/', (req, res) => {
   res.send('welcome to our art API');
@@ -15,15 +13,11 @@ app.get('/recommendation', (req, res) => {
   console.log('recommendation request');
 });
 
-app.get('/prompt/:promptText', (req, res) => {
-  // Users artwork attached to this prompt gets sent back here.
-  res.status(200).send('placeholder');
-});
-app.post('/prompt/:promptText', (req, res) => {
-  // User can add a post to a prompt here.
-  res.status(201).send('placeholder');
-});
-
+// Error handling for missing routes
 app.get('*', function (req, res) {
   res.status(404).send('what???');
+});
+
+app.listen(port, () => {
+  console.log(`${helpers.getServerGreeting()} & listening on port ${port}`);
 });
--- helpers.js
+++ helpers.js
@@ -2,4 +2,17 @@ const testHelpers = () => {
   console.log('I am a helper');
 };
 
-module.exports = { testHelpers };
+const getServerGreeting = () => {
+  const greetings = [
+    'Mixing the oil paints',
+    'Creating some happy accidents',
+    'Putting up the easel',
+    'Sharpening my pencils',
+    'Adjusting my beret',
+  ];
+
+  const randomIndex = Math.floor(Math.random() * greetings.length);
+  return greetings[randomIndex];
+};
+
+module.exports = { testHelpers, getServerGreeting };
