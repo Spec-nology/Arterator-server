78b3a01bb950c4c7012d253f10e15e2acfeff991
Dan Cooper <iamdanielcooper@users.noreply.github.com>
2022-06-24 20:39:38 +0100

    clean up and update readme

--- models/categories.js
+++ models/categories.js
@@ -1,53 +1,44 @@
 const db = require('../DB/dbConfig');
 const dbQueries = require('../DB/queries/dbQueries');
-
 const { formatTagData } = require('./helpers');
 
-// const init = async () => {
-//   await dbQueries.dbInit(db);
-//   await dbQueries.dbSeed(db);
-// };
-
 class CatagoriesAndTags {
-  constructor(data) {}
+    constructor(data) {}
 
-  static get getAllCategoriesAndTags() {
-    return new Promise(async (resolve, reject) => {
-      try {
-        // const result = await dbQueries.getTagsWithCategories(db);
-        const categories = await dbQueries.getAllCatagories(db);
-        const tags = await dbQueries.getAllTags(db);
+    static get getAllCategoriesAndTags() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const categories = await dbQueries.getAllCatagories(db);
+                const tags = await dbQueries.getAllTags(db);
 
-        const result = formatTagData(categories, tags);
+                resolve(formatTagData(categories, tags));
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
 
-        resolve(result);
-      } catch (error) {
-        reject(error);
-      }
-    });
-  }
+    static get getAllCategories() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const result = await dbQueries.getAllCatagories(db);
+                resolve(result);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
 
-  static get getAllCategories() {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const result = await dbQueries.getAllCatagories(db);
-        resolve(result);
-      } catch (error) {
-        reject(error);
-      }
-    });
-  }
-
-  static get getAllTags() {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const result = await dbQueries.getAllTags(db);
-        resolve(result);
-      } catch (error) {
-        reject(error);
-      }
-    });
-  }
+    static get getAllTags() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const result = await dbQueries.getAllTags(db);
+                resolve(result);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
 }
 
 module.exports = CatagoriesAndTags;
